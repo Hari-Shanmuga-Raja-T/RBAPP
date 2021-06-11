56301d8ca3c8d41bb43063a9a74d84f637b83ada
Hari Shanmuga Raja <harishanmugaraja@rently.com>
2021-06-11 19:03:10 +0530

    commit 1.0

---
+++ db/migrate/20210611102236_create_hotels.rb
@@ -0,0 +1,13 @@
+class CreateHotels < ActiveRecord::Migration[6.1]
+  def change
+    create_table :hotels do |t|
+      t.string :hotelid
+      t.string :hotelname
+      t.string :password
+      t.string :email
+      t.string :phno
+      t.string :address
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20210611102242_create_rooms.rb
@@ -0,0 +1,13 @@
+class CreateRooms < ActiveRecord::Migration[6.1]
+  def change
+    create_table :rooms do |t|
+      t.string :roomid
+      t.string :hotelid
+      t.string :roomno
+      t.string :status
+      t.string :price
+      t.string :type
+      t.timestamps
+    end
+  end
+end
